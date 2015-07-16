a08ec290240c000a6794264e1f6a715a253764c5
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-07-16 10:57:50 +0800

    add captcha module urlpattern

--- chuangplus/urls.py
+++ chuangplus/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'chuangplus.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    
-)
+
+    url(r'^api/captcha/', include('Captcha.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += [
     url(r'^.*$', 'app.views.index'),
 ]
