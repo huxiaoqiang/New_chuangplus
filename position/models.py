1c04bbd08c2ae75e8346d3c7fdf58ecd3c6d06b1
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-07-21 10:53:55 +0800

    add position module

---
+++ position/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from mongoengine import *
+# Create your models here
+
+TYPE = ('technology','product','design','operate','marketing','functions','others')
+STATUS = ('employing','hide')
+
+class Position(Document):
+    name = StringField(max_length=30)
+    type = StringField(max_length=15,choices=TYPE)
+    work_city=StringField(max_length=50)
+    work_address=StringField(max_length=50)
+    relese_time=DateTimeField(datetime.datetime.now)
+    end_time=DateTimeField()
+    position_descrition=StringField(max_length=500)
+    position_request=StringField(max_length=500)
+    daysperweek=IntField(default=3)
+    internship_time=IntField(default=1)
+    salary_max=IntField()
+    salary_min=IntField()
+    delivery_number=IntField()
+    status = StringField(max_length=15,choices=STATUS)
