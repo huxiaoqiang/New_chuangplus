66ab1cac1eba4fa7a74b18d3ec6b6cb6b59ba924
huxiaoqiang <hu-xq11@mails.tsinghua.edu.cn>
2015-07-29 21:24:08 +0800

    add user-related urls

--- account/urls.py
+++ account/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
-    url(r'^checkusername/$', views.check_username, name='check_username'),
-
-
+    url(r'^register$', views.register, name='register'),
+    url(r'^checkusername$', views.check_username, name='check_username'),
+    url(r'^login$',views.login, name='login'),
+    url(r'^logout$',views.logout, name='logout'),
+    url(r'^password/set$',views.set_password, name='set_password'),
+    url(r'^userinfo/get$',views.get_userinfo, name='get_userinfo'),
+    url(r'^userinfo/set$',views.set_userinfo, name='set_userinfo'),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
