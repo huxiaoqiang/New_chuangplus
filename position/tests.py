ad72db2284d806ebaddaa9f062bb697452921637
ryz1 <ryzcst@126.com>
2015-07-29 19:42:04 +0800

    test cases

--- position/tests.py
+++ position/tests.py
@@ -5,12 +5,26 @@ from .models import *
 from account.models import *
 
 def init_account():
-    
+    user1 = User(username = "ryz",password = "123456",email = "a@b.com",is_staff = 1)
+    user1.save
+    company1 = Companyinfo(username = "mycompany",User = user1)
+    company1.save()
 
 class TestPositionCreate:
     def setUp(self):
         self.c = Client()
         init_account()
         
-    def testCreate(self):
+    def testLogin(self):
+        res = self.c.post("/api/account/login/",{'username':'ryz','password' = '123456'})
+        self.assertEqual(res.context['error']['code'],1)
+    
+    def testLoginError(self):
+        res = self.c.post("/api/account/login/",{'username':'ryz','password' = '123457'})
+        self.assertEqual(res.context['error']['code'],108)
+    
+    def testCreatePosition(self):
+        res = self.c.post("/api/account/login/",{'username':'ryz','password' = '123456'})
+        self.assertEqual(res.context['error']['code'],1)
+        res = self.c.post("/api/position/create/",{})
         
