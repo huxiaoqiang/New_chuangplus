f65b80ccffc710b337a6354badf334162eae42c0
Yu Yan <yanyu02@gmail.com>
2015-08-02 12:15:17 +0800

    YY

--- filedata/urls.py
+++ filedata/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^upload', views.upload_file, name='upload_file'),
     url(r'^download',views.download_file,name='download_file'),
     url(r'^delete',views.delete_file,name='delete_file'),
+    url(r'^submit',views.submit_resume,name='submit_resume'),
 ]
