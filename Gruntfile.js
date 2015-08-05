3707d36bd6a9989dd806a2e8f8d70614c3f3df35
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-08-05 11:51:21 +0800

    修改gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,31 +10,26 @@ module.exports = function(grunt) {
                     './static/css/desktop/base.css': './static/less/desktop/base.less'
                 }
             }
-        },        
-        mbless: {
-            development: {
-                options: {
-                    path: ['./static/less/mobile'],
-                    yuicompress: true
-                },
-                files: {
-                    './static/css/mobile/base.css': './static/less/mobile/base.less'
-                }
-            }
         },
+//        less: {
+//            development: {
+//                options: {
+//                    path: ['./static/less/mobile'],
+//                    yuicompress: true
+//                },
+//                files: {
+//                    './static/css/mobile/base.css': './static/less/mobile/base.less'
+//                }
+//            }
+//        },
         watch: {
             files: "./static/less/desktop/*.less",
-            tasks: ['less']
-        },
-        watch: {
-            files: "./static/less/mobile/*.less",
             tasks: ['less']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.registerTask('mobile', ['mbless']);
 
     grunt.registerTask('default', ['less']);
 
