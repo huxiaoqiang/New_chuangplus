18a48db52f385334da20bc0802501c80b1706164
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-08-05 11:21:25 +0800

    修改gruntfile.js 分开桌面端和移动端

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,39 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    path: ['./static/less'],
+                    path: ['./static/less/desktop'],
                     yuicompress: true
                 },
                 files: {
-                    './static/css/base.css': './static/less/base.less'
+                    './static/css/desktop/base.css': './static/less/desktop/base.less'
+                }
+            }
+        },        
+        mbless: {
+            development: {
+                options: {
+                    path: ['./static/less/mobile'],
+                    yuicompress: true
+                },
+                files: {
+                    './static/css/mobile/base.css': './static/less/mobile/base.less'
                 }
             }
         },
         watch: {
-            files: "./static/less/*.less",
+            files: "./static/less/desktop/*.less",
+            tasks: ['less']
+        },
+        watch: {
+            files: "./static/less/mobile/*.less",
             tasks: ['less']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('mobile', ['mbless']);
 
     grunt.registerTask('default', ['less']);
+
 };
