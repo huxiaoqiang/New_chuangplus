e35874665175ff96e3c92be8656f251a83a309d9
Yu Yan <yanyu02@gmail.com>
2015-08-10 22:01:12 +0800

    account views test almost done

--- account/models.py
+++ account/models.py
@@ -37,7 +37,7 @@ class Userinfo(Document):
 
 # company position type
 FIELD = ('social','e-commerce','education','health_medical','culture_creativity','living_consumption','hardware','O2O','others')
-STAGE=('','seed','angel','A','B','C','D_plus')
+STAGE=('none','seed','angel','A','B','C','D_plus')
 class Companyinfo(Document):
     username = StringField(max_length=30,required=True)
     hr_cellphone = StringField(max_length=30,required=True)
@@ -46,8 +46,8 @@ class Companyinfo(Document):
     field = StringField(max_length=30,choices=FIELD)
     is_auth = BooleanField(default=False)
     auth_organization = StringField(max_length=100)
-    scale = IntField()  #0 newly established  1:rapid expansion 2:mature period
-    stage = StringField(max_length=10,choices=STAGE)
+    scale = IntField(default=0)  #0 newly established  1:rapid expansion 2:mature period
+    stage = StringField(max_length=10,choices=STAGE,default='none')
     homepage = URLField()
     financing = ListField(ReferenceField("Financing"))
     wechat = StringField(max_length=40)
@@ -67,16 +67,15 @@ class Companyinfo(Document):
     info_complete = BooleanField(default=False)
     date_joined = DateTimeField()
     update_time = DateTimeField()
-    user = ReferenceField(User)
+    user = ReferenceField(User,required=True)
 
 #project member table
 class Member(Document):
-    m_name= StringField(max_length=30)
-    m_position = StringField(max_length=30)
+    m_name= StringField(max_length=30,required=True)
+    m_position = StringField(max_length=30,required=True)
     m_introduction = StringField(max_length=1000)
     m_avatar = ReferenceField(File)
     company = ReferenceField(Companyinfo,required=True)
-
 
 AMOUNT=('ten','hundred','thousand','thousand_plus')
 class Financing(Document):
@@ -86,7 +85,7 @@ class Financing(Document):
     company = ReferenceField(Companyinfo,required=True)
 
 #user and company relationship
-#the user collect the company
+#the user likes the company
 class UC_Relationship(Document):
     company = ReferenceField(Companyinfo)
     user = ReferenceField(User)
