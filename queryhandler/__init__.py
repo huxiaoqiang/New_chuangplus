bafd14fbad4361d9d0f8f3751ff938b626e5900d
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-08-11 17:20:02 +0800

    微信request_handler 处理bug

--- queryhandler/__init__.py
+++ queryhandler/__init__.py
@@ -3,27 +3,28 @@ __author__ = "huxiaoqiang"
 import xml.etree.ElementTree as ET
 from django.utils.encoding import smart_str
 from wechatlib.base_support import auth
+from django.http import HttpResponse
 
 #todo add a wechat query handler entry
 #wechat request handler entry
 def request_handler(request):
     if request.method == "GET":
-        signature=request.GET['signature']
-        timestamp=request.GET['timestamp']
-        nonce=request.GET['nonce']
-        echostr=request.GET['echostr']
+        signature=request.GET.get('signature','')
+        timestamp=request.GET.get('timestamp','')
+        nonce=request.GET.get('nonce','')
+        echostr=request.GET.get('echostr','')
         if not auth(signature, timestamp, nonce):
             print '!!!!! Check weixin signature failed !!!!!'
-            return ''
+            return HttpResponse('')
         else:
-            return echostr
+            return HttpResponse(echostr)
     elif request.method == "POST":
-        signature=request.POST.get('signature','')
-        timestamp=request.POST.get('timestamp','')
-        nonce=request.POST.get('nonce','')
+        signature=request.POST.get(u'signature','')
+        timestamp=request.POST.get(u'timestamp','')
+        nonce=request.POST.get(u'nonce','')
         if not auth(signature, timestamp, nonce):
             print '!!!!! Check weixin signature failed !!!!!'
-            return ''
+            return HttpResponse('')
 
 
 
