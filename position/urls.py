a07cc225dbff9b46929207db0248330e7b772798
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-09-11 14:47:24 +0800

    添加收藏列表中的，删除失效岗位函数和一键投递功能

--- position/urls.py
+++ position/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^(?P<position_id>.*?)/get$', views.get_position, name='get_position'),
     url(r'^(?P<position_id>.*?)/get_with_company$', views.get_position_with_company, name='get_position_with_company'),
     url(r'^(?P<position_id>.*?)/submit$', views.submit_resume, name='submit_resume'),
+
     url(r'^search$', views.search_position, name='search_position'),
     url(r'^company/(?P<company_id>.*?)/list$',views.get_company_position_list,name='get_company_position_list'),
-    url(r'^init$',tests.init_account),
     url(r'^(?P<position_id>.*?)/check_submit',views.check_submit,name='check_submit'),
     url(r'^emailresume',views.email_resume,name='email_resume'),
 
