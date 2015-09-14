d5b204f15d1ea489b94db390cf86d5c288f33ebc
wt9431 <654354746@qq.com>
2015-09-14 15:27:17 +0800

    主页更新

--- static/js/controllers_desktop.js
+++ static/js/controllers_desktop.js
@@ -51,7 +51,7 @@ angular.module('chuangplus.controllers', []).
                     }
                 });
         };
-        //tab active control
+       //tab active control
         $scope.homepage_active = function(){
             $scope.homepage = true;
             $scope.position = $scope.company = $scope.resume = false;
