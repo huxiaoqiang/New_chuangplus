ffe004f6f7f082293da39934f6751e3598b814b4
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-09-21 00:41:26 +0800

    添加验证码功能

--- Captcha/models.py
+++ Captcha/models.py
@@ -1,3 +1,9 @@
+#coding=utf-8
 from django.db import models
+from mongoengine import *
 
-# Create your models here.
+class InvitationCode(Document):
+    code = StringField(max_length=30)
+    status = BooleanField(default=False)
+    def __str__(self):
+        return self.usercode
--- Captcha/urls.py
+++ Captcha/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^image/$', views.image, name='image'),
     url(r'^check/$', views.check, name='check'),
+    url(r'^check_ic$',views.check_invitation_code,name='check_invitation_code'),
+    url(r'^register_invitation_code$',views.register_invitation_code,name='register_invitation_code$'),
+    url(r'^invitationcode$',views.generate_invitation_code,name='generate_invitation_code'),
 ]
