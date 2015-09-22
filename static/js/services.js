2650410e5b8f7fefb2fd96033b63f3cb8fd5d5f9
mxbhsa <matnt2008@126.com>
2015-09-22 17:01:08 +0800

    js

--- static/js/services.js
+++ static/js/services.js
@@ -222,7 +222,7 @@ angular.module('chuangplus.services', []).
                         orig_src.width = 360;
                         image_target.width = 360;
                         resize_canvas.width = 360;
-                        resize_canvas.height = orig_src.src.;
+                        resize_canvas.height = 360 / orig_src.width * orig_src.height;
                         // Wrap the image with the container and add resize handles
                         $(image_target).wrap('<div class="resize-container"></div>')
                         .before('<span class="resize-handle resize-handle-nw"></span>')
