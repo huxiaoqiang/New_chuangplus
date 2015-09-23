1b3536e0e9e8622783384c9cbd873c1e9a1c5536
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2015-09-23 20:11:40 +0800

    删除职位信息

--- static/js/controllers_desktop.js
+++ static/js/controllers_desktop.js
@@ -1499,7 +1499,7 @@ angular.module('chuangplus.controllers', []).
         $scope.company_id = $routeParams.company_id;
         $scope.position_list = {};
         $scope.delete_position = function(index){
-            $http.post(urls.api+"/position/"+$scope.position_list[index]['_id']['$oid']+"/delete").
+            $http.get(urls.api+"/position/"+$scope.position_list[index]['_id']['$oid']+"/delete").
               success(function(data){
                     if(data.error.code == 1){
                         $scope.get_position_list();
