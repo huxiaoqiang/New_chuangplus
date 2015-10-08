214e272f56a68cb7c765d0763567fdb62cadec17
mxbhsa <matnt2008@126.com>
2015-10-09 00:56:57 +0800

    bug

--- static/js/mobile/controllers_mobile.js
+++ static/js/mobile/controllers_mobile.js
@@ -1964,8 +1964,8 @@ angular.module('chuangplus_mobile.controllers', [])
         };
     }])
 
-    .controller('MB_UserInfoEditCtrl', ['$scope', '$http', 'urls', 'CsrfService', '$routeParams', 'NoticeService', 'UserService','ErrorService', '$rootScope',
-    function($scope, $http, urls, $csrf, $routeParams, $notice, $user, $errMsg, $rootScope ) {
+    .controller('MB_UserInfoEditCtrl', ['$scope', '$http', 'urls', 'CsrfService', '$routeParams', 'NoticeService', 'UserService','ErrorService', '$rootScope', '$location',
+    function($scope, $http, urls, $csrf, $routeParams, $notice, $user, $errMsg, $rootScope, $location ) {
         console.log('MB_UserInfoEditCtrl');
         if($user.username == undefined)
             window.location.href='/mobile/login';
