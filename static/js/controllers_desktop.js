4853ab515ee5de9a7774fe346d765fb3bf50bc3f
mxbhsa <matnt2008@126.com>
2015-10-30 15:10:03 +0800

    已收藏时文字变化

--- static/js/controllers_desktop.js
+++ static/js/controllers_desktop.js
@@ -1708,12 +1708,16 @@ angular.module('chuangplus.controllers', []).
             });
         };
         $scope.change_in = function(){
-            if(!$scope.favor_exist)
+            if(!$scope.favor_exist){
                 $scope.img_src = "/static/image/icon/shoucang2-01.png";
+                $scope.post_value = '取消收藏';
+            }
         };
         $scope.change_out = function(){
-            if(!$scope.favor_exist)
+            if(!$scope.favor_exist){
                 $scope.img_src = "/static/image/icon/shoucang-01.png";
+                $scope.post_value = '已收藏';
+            }
         };
         $scope.change2_in = function(){
             $scope.img2_src="/static/image/icon/weixin2-01.png";
@@ -2359,10 +2363,17 @@ angular.module('chuangplus.controllers', []).
             });
         };
         $scope.change_in = function(){
-            $scope.img_src = "/static/image/icon/shoucang2-01.png";
+            if(!$scope.favored){
+                $scope.img_src = "/static/image/icon/shoucang2-01.png";
+            }
+            else
+                $scope.post_value = '取消收藏';
         };
         $scope.change_out = function(){
-            $scope.img_src = "/static/image/icon/shoucang-01.png";
+            if(!$scope.favored)
+                $scope.img_src = "/static/image/icon/shoucang-01.png";
+            else
+                $scope.post_value = '收藏';
         };
         $scope.change2_in = function(){
             $scope.img2_src="/static/image/icon/weixin2-01.png";
